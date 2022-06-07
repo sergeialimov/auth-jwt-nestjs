36664a1dffe45f05cb43c47afde366252d1e84c0
sergeialimov <8912652@gmail.com>
2022-06-07 13:26:44 +0300

    add users[] and methods in user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [
+    {
+      id: 0,
+      name: 'Bob',
+      email: 'bob@gmail.com',
+      password: 'bobPass',
+    },
+
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'johnPass',
+    },
+
+    {
+      id: 2,
+      name: 'Gary',
+      email: 'gary@gmail.com',
+      password: 'garyPass',
+    },
+  ];
+
+  findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find((user) => user.email === email);
+    if (user) {
+      return Promise.resolve(user);
+    }
+    return undefined;
+  }
+  findOne(id: number): Promise<User | undefined> {
+    const user = this.users.find((user) => user.id === id);
+    if (user) {
+      return Promise.resolve(user);
+    }
+    return undefined;
+  }
+}
