a076a31793b4928c291ea6e781ba55b19619a64e
sergeialimov <8912652@gmail.com>
2022-06-09 06:07:16 +0300

    implement newRefreshAndAccessToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { sign } from 'crypto';
+import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
+import RefreshToken from './entities/refresh-token.entity';
 
 @Injectable()
 export class AuthService {
@@ -7,6 +10,32 @@ export class AuthService {
 
   constructor(private readonly userService: UserService) {
     this.userService = userService();
+  }
+
+  private async newRefreshAndAccessToken(
+    user: User,
+    values: {
+      userAgent: string;
+      ipAddress: string;
+    },
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const refreshObject = new RefreshToken({
+      id:
+        this.refreshToken.length === 0
+          ? 0
+          : this.refreshTokens[this.refreshTokens.lengh - 1].id + 1,
+      ...values,
+      userId: user.id,
+    });
+    // add refreshObject in your db in real app
+    this.refreshTokens.push(refreshObject);
+
+    return {
+      refreshToken: refreshObject.sign(),
+      accesToken: sign({ userId: user.id }, process.env.ACCESS_SECRET, {
+        expiresIn: '1h',
+      }),
+    };
   }
 
   async login(
