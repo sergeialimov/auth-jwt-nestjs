161d894cb07339442405f5597d72cc82db98f8c3
sergeialimov <8912652@gmail.com>
2022-06-12 21:51:10 +0300

    close constructor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,32 +3,30 @@ import { AuthService } from './auth.service';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { LoginDto } from './dto/login.dto';
 
-
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    // todo: create the LoginDTo
-    async login(@Req() request, @Ip() ip: string, @Body() body: LoginDto) {
-      // getting the username and ip address from @Req decorator and @Ip decorator imported at the top
+  @Post('login')
+  // todo: create the LoginDTo
+  async login(@Req() request, @Ip() ip: string, @Body() body: LoginDto) {
+    // getting the username and ip address from @Req decorator and @Ip decorator imported at the top
 
-      return this.authService.login(body.email, body.password, {
-        ipAddress: ip,
-        userAgent: request.headers['user-agent'],
-      });
-    }
+    return this.authService.login(body.email, body.password, {
+      ipAddress: ip,
+      userAgent: request.headers['user-agent'],
+    });
+  }
 
-    @Post('refresh')
-    // todo: create refreshTokenDto
-    async refreshToken(@Body() body: RefreshTokenDto) {
-      return this.authService.refresh(body.refreshToken);
-    }
+  @Post('refresh')
+  // todo: create refreshTokenDto
+  async refreshToken(@Body() body: RefreshTokenDto) {
+    return this.authService.refresh(body.refreshToken);
+  }
 
-    @Delete('logout')
-    // todo: create refreshTokenDto
-    async logout(@Body() body: RefreshTokenDto) {
-      return this.authService.logout(body.refreshToken);
-    }
+  @Delete('logout')
+  // todo: create refreshTokenDto
+  async logout(@Body() body: RefreshTokenDto) {
+    return this.authService.logout(body.refreshToken);
   }
 }
